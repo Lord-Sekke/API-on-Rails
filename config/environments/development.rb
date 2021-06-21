44b8761c9c5d46930e251e7e9a23217f1ee59014
Lord-Sekke <86236909+Lord-Sekke@users.noreply.github.com>
2021-06-21 18:51:55 +0500

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  #host configuration for educative platform
+  config.hosts << "81674631jk52n.educative.run"
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
